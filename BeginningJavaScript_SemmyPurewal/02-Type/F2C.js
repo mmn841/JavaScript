e9aa7f928096d7fb25944e9c97552a48fa857356
mmn841 <12266113+mmn841@users.noreply.github.com>
2018-03-29 17:02:27 +0530

    Chapter-Type
    Commited code practiced for the chapter "Types"

---
+++ BeginningJavaScript_SemmyPurewal/02-Type/F2C.js
@@ -0,0 +1,7 @@
+//Convert fahrenheit to celsius
+var F2C = function(f) {
+    return ((f * 9) / 5 ) + 32;
+}
+
+var f = 0;
+console.log("Converting " + f + " fahrenheit gives: " + F2C(f) + " celsius");

f42952d27b934e1a1a2f16e4d521b34df4ee0b53
madhuran <madhuran@juniper.net>
2018-03-29 14:53:00 +0530

    Revert "Function"
    This reverts commit a35e99fec11c430e019fb9fe38d90430da0c9c7c.

--- BeginningJavaScript_SemmyPurewal/01-Functions/AddThree.js
+++
@@ -1,7 +0,0 @@
-var addThree = function(a, b, c) {
-    var sum = a + b + c;
-    console.log("The calculated sum is: " + sum);
-    return sum;
-}
-
-console.log(addThree(1,2,3))
--- BeginningJavaScript_SemmyPurewal/01-Functions/ToCardString.js
+++
@@ -1,8 +0,0 @@
-var toCardString = function(cardValue, cardType) {
-    var result = cardValue + " of " + cardType;
-    return result;
-}
-
-console.log(toCardString("ace","spades"));
-console.log(toCardString("two","diamonds"));
-console.log(toCardString("queen", "hearts"));
